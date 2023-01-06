79a9ca8ce0f75a780cd5267bcb15d5560db8d291
Ynabin01 <nabin.01least@gmail.com>
2023-01-06 11:26:00 +0545

    today4

--- resources/views/website/clients.blade.php
+++ resources/views/website/clients.blade.php
@@ -1,4 +1,4 @@
- <div class="branch-address">
+ {{-- <div class="branch-address">
     <div class="container">
 
         <div class="row">
@@ -21,4 +21,45 @@
 
     </div>
 </div>   
+ --}}
 
+ <div class="branch-address">
+    <div class="container">
+        <div class="row">
+            <div class="address-slider">
+                <div class="item">
+                    <div class="wrapper ourClient_img">
+                        <img src="images/ourClient/19.jpg" alt="">
+                    </div> <!-- /.wrapper -->
+                </div>
+
+                <div class="item">
+                    <div class="wrapper ourClient_img">
+                        <img src="images/ourClient/12.jpg" alt="">
+                    </div> <!-- /.wrapper -->
+                </div>
+                <div class="item">
+                    <div class="wrapper ourClient_img">
+                        <img src="images/ourClient/18.jpg" alt="">
+                    </div> <!-- /.wrapper -->
+                </div>
+                <div class="item">
+                    <div class="wrapper ourClient_img">
+                        <img src="images/ourClient/18.jpg" alt="">
+                    </div> <!-- /.wrapper -->
+                </div>
+                <div class="item">
+                    <div class="wrapper ourClient_img">
+                        <img src="images/ourClient/19.jpg" alt="">
+                    </div> <!-- /.wrapper -->
+                </div>
+                <div class="item">
+                    <div class="wrapper ourClient_img">
+                        <img src="images/ourClient/1.jpg" alt="">
+                    </div> <!-- /.wrapper -->
+                </div>
+
+            </div> <!-- /.address-slider -->
+        </div>
+    </div> <!-- /.container -->
+</div> <!-- /.branch-address -->
--- resources/views/website/normal.blade.php
+++ resources/views/website/normal.blade.php
@@ -2,36 +2,34 @@
 
 @section('content')
     <!--
-               =============================================
-                Theme Inner Banner
-               ==============================================
-               -->
- 
-        <div class="theme-inner-banner section-spacing">
-            <div class="overlay">
-                <div class="container">
-                    <h2>Prokleen Services LLC</h2>
-                </div> <!-- /.container -->
-            </div> <!-- /.overlay -->
-        </div> <!-- /.theme-inner-banner -->
-     
-        <div class="about-compnay-two no-bg section-spacing">
-            <div class="overlay">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 col-12 order-lg-first">
-                            <img src="{{ $normal->banner_image }}" alt="" class="left-img">
-                        </div>
-                        <div class="col-lg-6 col-12 text order-lg-last">
-                            <div class="theme-title-one">
-                                <h2>@php echo $normal->caption; @endphp</h2>
-                            </div> <!-- /.theme-title-one -->
-                            <p>@php echo $normal->long_content; @endphp
-                            </p>
-                        </div> <!-- /.col- -->
-                    </div> <!-- /.row -->
-                </div> <!-- /.container -->
-            </div> <!-- /.overlay -->
-        </div> <!-- /.about-compnay-two -->
-   
+                   =============================================
+                    Theme Inner Banner
+                   ==============================================
+                   -->
+
+    <div class="theme-inner-banner section-spacing">
+        <div class="overlay">
+            <div class="container">
+                <h2>Prokleen Services LLC</h2>
+            </div> <!-- /.container -->
+        </div> <!-- /.overlay -->
+    </div> <!-- /.theme-inner-banner -->
+
+    <div class="about-compnay-two no-bg section-spacing">
+        <div class="overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-12 order-lg-first">
+                        <img src="{{ $normal->banner_image }}" alt="" class="left-img">
+                    </div>
+                    <div class="col-lg-6 col-12 text order-lg-last">
+                        <div class="theme-title-one">
+                            <h2>@php echo $normal->caption; @endphp</h2>
+                        </div> <!-- /.theme-title-one -->
+                        @php echo $normal->short_content; @endphp
+                    </div> <!-- /.col- -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.overlay -->
+    </div> <!-- /.about-compnay-two -->
 @endsection
